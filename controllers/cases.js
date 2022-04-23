deaf09a363a869c4bddf2429deeedda2d42b7a61
henriquewho <henriquegfonseca89@gmail.com>
2022-04-23 12:34:37 -0300

    Added the getCasesByDay route, which will return _id:location and an array with objets that have as properties the name of a variant and the value for that day

--- controllers/cases.js
+++ controllers/cases.js
@@ -1,7 +1,31 @@
+/* 
+GET]/cases/:date/count: Listar todos os registros da base de dados no dia selecionado, agrupados por país e separados por variante.
+
+[GET]/cases/:date/cumulative: Listar todos os registros da base de dados, retornando a soma dos casos registrados de acordo com a data selecionada, agrupados por país e separados por variante.
+*/
+
 const Entry = require('../models/entry');
 
-const getCasesByDay = (req, res) => {
-    res.end(`date: ${req.date}`)
+const getCasesByDay = async (req, res) => {
+    const filter = {date: new Date(req.date)}; 
+
+    const entries = await Entry.aggregate()
+    .match(filter)
+    .group({"_id": {
+        "location": "$location", 
+        "variant": "$variant", 
+        "num_sequences": "$num_sequences"
+    }})
+    .group({
+        "_id": "$_id.location", 
+        "variants": {
+            "$push": {
+                    "variant": "$_id.variant", "num": "$_id.num_sequences"
+            }
+        }
+    })
+
+    res.json(entries)
 }
 
 const getCumulativeCases = (req, res) => {
